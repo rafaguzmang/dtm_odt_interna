82b431239eb03b38a8670ea67353cfbcb08cd08c
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-10-10 12:00:43 -0600

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name":"dtm_odt_interna",
+    "description":"Control de ordenes de trabajo interna",
+    # 'depends': ['base', 'mail','dtm_servicios_externos'],
+    "data":[
+        #Security
+        'security/ir.model.access.csv',
+        #Views
+        'views/dtm_odt_interna_view.xml',
+        # 'views/dtm_npi_view.xml',
+        #Menú
+        # 'views/dtm_menu.xml',
+
+        ],
+    'license': 'LGPL-3',
+}
---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from . import dtm_odt_interna
